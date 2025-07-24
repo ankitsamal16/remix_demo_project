d6ef2760dec5b6e01f5eccef371ef9eb34d1aec4
AnkitSamal16 <126022706+AnkitSamal16@users.noreply.github.com>
2025-07-24 21:08:22 +0530

    new_commit

--- app/components/ChatInput.tsx
+++ app/components/ChatInput.tsx
@@ -20,18 +20,3 @@ export default function ChatInput() {
     </div>
   );
 }
-
-
-// export default function ChatInput() {
-//     return (
-//         <div className="absolute bottom-0 w-5/6 flex justify-center py-4 bg-white border-t">
-//           <div className="flex items-center w-[90%] max-w-4xl border-2 border-gray-400 rounded-md px-4 py-2 shadow-sm">
-//             <input type="text" placeholder="Type your message..." className="bg-white flex-1 px-2 py-1 outline-none" />
-//             <span className="text-sm pr-4 text-gray-600">0/4000</span>
-//           </div>
-//             <button className="text-white hover:bg-teal-800 text-xl bg-teal-600">
-//               <i className="ri-telegram-2-line"></i>
-//             </button>
-//         </div>
-//     )
-// }
